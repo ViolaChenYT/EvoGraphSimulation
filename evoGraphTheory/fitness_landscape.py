fafc26b4174743760612848f297bb9b99da4b6bd
Yitao Chen <violachenyt@login04.osgconnect.net>
2022-03-25 00:10:18 -0500

    cluster

--- evoGraphTheory/fitness_landscape.py
+++ evoGraphTheory/fitness_landscape.py
@@ -16,7 +16,7 @@ from no_visual import*
 VISUAL = False
 PLOT = True
 '''PARAMETERS'''
-N_TRIALS = 5 # per run
+N_TRIALS = 20 # per run
 n_keep = 50 # no. of frames to record
 
 # Evolution parameters
