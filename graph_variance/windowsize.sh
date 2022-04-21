288a2b4870eca52145c3626df785894c4be145a4
ViolaChenYT <yitaoche@andrew.cmu.edu>
2022-04-20 23:47:36 -0400

    local before pulling

--- graph_variance/windowsize.sh
+++ graph_variance/windowsize.sh
@@ -2,8 +2,8 @@
 
 C=0.05
 
-for i in {1..100}
+for i in {0..99}
 do
 	param=$(echo "scale=4;$C*$i" |bc)
-	./runsim.sh wellmixed 50000 1 $param 0.05 >> ./results/wellmixed_diffs_var0.05.txt
+	./runsim.sh wellmixed 50000 1 $param 0.1 >> ./results/wellmixed_diffs_var.1.txt
 done
