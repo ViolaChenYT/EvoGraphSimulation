472d3c91a70e9660b5a8607452cec5208df1b6a7
Yitao Chen <violachenyt@login04.osgconnect.net>
2022-04-22 14:35:04 -0500

    cluster commit before pull

--- graph_variance/variance.sh
+++ graph_variance/variance.sh
@@ -2,9 +2,31 @@
 
 C=0.05
 
+ID=star
+
+GRAPH_FILE=graphs/$ID.txt
+
+if [[ ! -f $GRAPH_FILE ]]; then
+	echo -e "graph file not found"
+	exit;
+fi
+
+OUTPUT_FILE=results/${FIT_VAR}.txt
+
+NUM_RUNS=100000
+
+DIST="poisson"
+
+FIT_ADV=0.05
+
+
 for i in {0..99}
 do
 	param=$(echo "scale=4;$C*$i" |bc)
-	./runsim.sh star 80000 "binom" 5 0.05 $param >> ./results/binom_variance_star.txt
+	FIT_VAR=$param
+	
+	BASE_FIT=$param
+
+	./sim.out $GRAPH_FILE $OUTPUT_FILE $NUM_RUNS $DIST $BASE_FIT $FIT_ADV $FIT_VAR >> poisson_variance_star.txt 
 done
 
