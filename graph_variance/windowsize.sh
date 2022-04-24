5922c5ce442c70a7e52cf6eaf1a4284866b69376
Viola Chen <yitaoche@andrew.cmu.edu>
2022-04-24 01:08:58 -0400

    local

--- graph_variance/windowsize.sh
+++ graph_variance/windowsize.sh
@@ -5,5 +5,5 @@ C=0.05
 for i in {1..100}
 do
 	param=$(echo "scale=4;$C*$i" |bc)
-	./runsim.sh star 10000 "binom" 5 0 $param >> ./results/star_binom_var.txt
+	./runsim.sh star 50000 "binom" 5 0 $param >> ./results/star_binom_s0.txt
 done
