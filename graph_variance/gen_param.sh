cdc78c2aaf36bc3a5facf6b7a2b2dfac03558e50
Yitao Chen <violachenyt@login04.osgconnect.net>
2022-06-04 21:08:34 -0500

    param graph result

--- graph_variance/gen_param.sh
+++ graph_variance/gen_param.sh
@@ -4,6 +4,6 @@ echo "" > ${model}_param.in
 for i in {1..8000}
 do
 	res=$(((i-1)%800))
-	echo "param_graphs/$res.txt   ${model}_result/${i}.txt" >> ${model}_param.in
+	echo "param_graphs/$res.txt   ${model}_result1/${i}.txt" >> ${model}_param.in
 	# echo "${model}/${i}.txt" >> ${model}_param.in
 done
