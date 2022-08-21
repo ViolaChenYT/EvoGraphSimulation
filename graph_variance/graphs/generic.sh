7ed35d0d09273a2ef1c8d1546f3a73c2a9fc9f7b
Viola Chen <yitaoche@andrew.cmu.edu>
2022-08-21 20:50:24 +0800

    changed generic script to allow batch

--- graph_variance/graphs/generic.sh
+++ graph_variance/graphs/generic.sh
@@ -1,50 +1,56 @@
 #!/bin/bash
 
 input_dir=$1
-output_dir="res_${input_dir}"
-if [[ ! -d "$output_dir" ]]; then
-	mkdir $output_dir
-fi 
-script="$input_dir.sh"
-param_file="$input_dir.param.in"
-printf "" >> $param_file
-let cnt=0
-# generate params
-for file in $input_dir/*
+
+for idx in {1..5}
 do
-  # echo $file
-  cnt=$((cnt+1))
-  id=$(sed 's/.*\/\(.*\)\..*/\1/' <<< "$file")
-  printf $file >> $param_file
-  printf "\t$output_dir/$id.txt\n" >> $param_file
+  output_dir="res_${input_dir}$idx"
+  script="$input_dir$idx.sh"
+  param_file="$input_dir$idx.param.in"
+  submit_file="$input_dir$idx.submit"
+
+  if [[ ! -d "$output_dir" ]]; then
+    mkdir $output_dir
+  fi 
+  
+  printf "" >> $param_file
+  let cnt=0
+  # generate params
+  for file in $input_dir/*
+  do
+    # echo $file
+    cnt=$((cnt+1))
+    id=$(sed 's/.*\/\(.*\)\..*/\1/' <<< "$file")
+    printf $file >> $param_file
+    printf "\t$output_dir/$id.txt\n" >> $param_file
+  done
+
+  # write script to be run
+  printf "#!/bin/bash\n" > $script
+  echo 'i=$1' >> $script
+  echo 'i=$((i+1))' >> $script
+  echo 'x=$(sed "${i}q;d"' $param_file "| awk '{print \$1}')" >> $script
+  echo 'y=$(sed "${i}q;d"' $param_file "| awk '{print \$2}')" >> $script
+  echo "dist=binom" >> $script
+  echo './gph.out $x $y 100000 $dist 0.1 0.0 0.9' >> $script
+  chmod +x $script
+
+  # write the submit file
+  echo "Universe = vanilla" > $submit_file
+  echo "Executable = $script" >> $submit_file
+  echo 'Arguments = $(process)' >> $submit_file
+
+  echo 'Error = log/osgJob$(process).err' >> $submit_file
+  echo 'Output = log/osgJob$(process).out' >> $submit_file
+  echo 'Log = log/osgJob$(process).log' >> $submit_file
+
+  echo "should_transfer_files = YES" >> $submit_file
+  echo "when_to_transfer_output = ON_EXIT" >> $submit_file
+  echo "transfer_input_files=gph.out, $input_dir, $param_file, $output_dir" >> $submit_file
+  echo "transfer_output_files=$output_dir" >> $submit_file
+
+  echo "Queue $cnt" >> $submit_file
+
+  condor_submit $submit_file
 done
-
-# write script to be run
-printf "#!/bin/bash\n" > $script
-echo 'i=$1' >> $script
-echo 'i=$((i+1))' >> $script
-echo 'x=$(sed "${i}q;d"' $param_file "| awk '{print \$1}')" >> $script
-echo 'y=$(sed "${i}q;d"' $param_file "| awk '{print \$2}')" >> $script
-echo "dist=binom" >> $script
-echo './gph.out $x $y 100000 $dist 0.1 0.0 0.9' >> $script
-chmod +x $script
-
-# write the submit file
-submit_file="$input_dir.submit"
-echo "Universe = vanilla" > $submit_file
-echo "Executable = $script" >> $submit_file
-echo 'Arguments = $(process)' >> $submit_file
-
-echo 'Error = log/osgJob$(process).err' >> $submit_file
-echo 'Output = log/osgJob$(process).out' >> $submit_file
-echo 'Log = log/osgJob$(process).log' >> $submit_file
-
-echo "should_transfer_files = YES" >> $submit_file
-echo "when_to_transfer_output = ON_EXIT" >> $submit_file
-echo "transfer_input_files=gph.out, $input_dir, $param_file, $output_dir" >> $submit_file
-echo "transfer_output_files=$output_dir" >> $submit_file
-
-echo "Queue $cnt" >> $submit_file
-
-condor_submit $submit_file
 #
