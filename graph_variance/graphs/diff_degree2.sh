bb01a1417e1fbfe043e6a6110112eb8ad0992816
Yitao Chen <violachenyt@login04.osgconnect.net>
2022-08-29 22:21:04 -0500

    result from ytd

--- graph_variance/graphs/diff_degree2.sh
+++ graph_variance/graphs/diff_degree2.sh
@@ -4,4 +4,4 @@ i=$((i+1))
 x=$(sed "${i}q;d" diff_degree2.param.in | awk '{print $1}')
 y=$(sed "${i}q;d" diff_degree2.param.in | awk '{print $2}')
 dist=binom
-./gph.out $x $y 200000 $dist 0.1 0.0 0.9
+./gph.out $x $y 100000 $dist 0.1 0.0 0.9
