0b8b70da048b4e0d6ce7bdce97c01284fb477efc
Yitao Chen <violachenyt@login04.osgconnect.net>
2022-09-19 16:57:10 -0500

    pushing results

---
+++ graph_variance/graphs/90_10_deg_diff_3reg1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+i=$1
+i=$((i+1))
+x=$(sed "${i}q;d" 90_10_deg_diff_3reg1.param.in | awk '{print $1}')
+y=$(sed "${i}q;d" 90_10_deg_diff_3reg1.param.in | awk '{print $2}')
+dist=binom
+./gph.out $x $y 150000 $dist 0.1 0.0 0.9
