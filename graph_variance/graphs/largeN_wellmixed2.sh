a199bfc10ac05174539bef1c216f9cbdfac3a13e
Yitao Chen <violachenyt@login04.osgconnect.net>
2022-11-21 07:37:02 -0600

    wellmixed with more runs

--- graph_variance/graphs/largeN_wellmixed2.sh
+++ graph_variance/graphs/largeN_wellmixed2.sh
@@ -4,4 +4,4 @@ i=$((i+1))
 x=$(sed "${i}q;d" largeN_wellmixed2.param.in | awk '{print $1}')
 y=$(sed "${i}q;d" largeN_wellmixed2.param.in | awk '{print $2}')
 dist=binom
-./gph.out $x $y 10000 $dist 0.0 0.0 0.9
+./gph.out $x $y 1000000 $dist 0.0 0.0 0.9
