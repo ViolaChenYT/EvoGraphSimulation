9c40ebc2d538ce2a7900e4bcd0943e73336863b0
Viola Chen <yitaoche@andrew.cmu.edu>
2022-12-07 17:30:54 +0800

    some small graphs

--- graph_variance/graphs/graph10/extract_graph.py
+++ graph_variance/graphs/graph10/extract_graph.py
@@ -3,15 +3,25 @@ import sys,os
 
 
 for filename in os.listdir("./"):
-  if len(filename) < 5:
-    continue
+  if not filename.endswith(".txt"): continue
   idx = (filename.split(".")[0]).split("_")[1]
-  with open(filename) as f:
-    lines = f.readlines()
-    print(lines)
+  if int(idx) > 100: continue
   path = f"group_{idx}_graphs"
-  if not os.path.exists(path):
-    os.makedirs(path)
-    print(filename,idx)
+  # if not os.path.exists(path):
+  #   os.makedirs(path)
+    # print(filename,idx)
+    
+  with open(filename) as f:
+    lines = "".join(f.readlines())
+    lines = lines.split("\nGraph ")
+    for line in lines:
+      graph_id = line.split(",")[0]
+      # print(graph_id)
+      el = "  ".join(line.split("\n")[2:])
+      el = el.split("  ")[:-1]
+      with open(f"{path}/{graph_id}.txt","w") as gf:
+        gf.write("\n".join(el))
+      gf.close()
+  
   f.close()
 
