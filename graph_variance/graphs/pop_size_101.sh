a8479a74fbd6d3251915eee71ab2def77613819a
Yitao Chen <violachenyt@login04.osgconnect.net>
2022-12-07 00:45:42 -0600

    more runs

--- graph_variance/graphs/pop_size_101.sh
+++ graph_variance/graphs/pop_size_101.sh
@@ -4,4 +4,4 @@ i=$((i+1))
 x=$(sed "${i}q;d" pop_size_101.param.in | awk '{print $1}')
 y=$(sed "${i}q;d" pop_size_101.param.in | awk '{print $2}')
 dist=binom
-./gph $x $y 10000 $dist 0.1 0.0 0.9
+./gph $x $y 100000 $dist 0.1 0.0 0.9
