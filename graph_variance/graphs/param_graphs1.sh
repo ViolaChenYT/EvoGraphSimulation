a83821401c3c33733e1d7a2b492013ffb17b51eb
Yitao Chen <violachenyt@login04.osgconnect.net>
2023-02-13 03:10:17 -0600

    more results including wellmixed

--- graph_variance/graphs/param_graphs1.sh
+++ graph_variance/graphs/param_graphs1.sh
@@ -4,4 +4,4 @@ i=$((i+1))
 x=$(sed "${i}q;d" param_graphs1.param.in | awk '{print $1}')
 y=$(sed "${i}q;d" param_graphs1.param.in | awk '{print $2}')
 dist=binom
-./gph $x $y 100000 $dist 0.1 0.0 0.9
+./gph $x $y 500000 $dist 0.1 0.0 0.9
