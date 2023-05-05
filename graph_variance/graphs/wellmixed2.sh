bbe87d53b7b779d2d7ca35f21d7cff92627e7175
Yitao Chen <violachenyt@login04.osgconnect.net>
2023-05-05 15:24:15 -0500

    different s

--- graph_variance/graphs/wellmixed2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-i=$1
-i=$((i+1))
-x=$(sed "${i}q;d" wellmixed2.param.in | awk '{print $1}')
-y=$(sed "${i}q;d" wellmixed2.param.in | awk '{print $2}')
-dist=binom
-./gph $x $y 500000 $dist 0.1 0.0 0.9
