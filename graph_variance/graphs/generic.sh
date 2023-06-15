2f465616e815f6e6c8feeef2fab181d96fbd7f22
Yitao Chen <violachenyt@login04.osgconnect.net>
2023-06-15 07:29:23 -0500

    results

--- graph_variance/graphs/generic.sh
+++ graph_variance/graphs/generic.sh
@@ -4,7 +4,7 @@ input_dir=$1
 
 for idx in {1..5}
 do
-  output_dir="rslt001_${input_dir}${idx}"
+  output_dir="result020_${input_dir}${idx}"
   script="$input_dir${idx}.sh"
   param_file="$input_dir${idx}.param.in"
   submit_file="$input_dir${idx}.submit"
@@ -35,7 +35,7 @@ do
   echo 'x=$(sed "${i}q;d"' $param_file "| awk '{print \$1}')" >> $script
   echo 'y=$(sed "${i}q;d"' $param_file "| awk '{print \$2}')" >> $script
   echo "dist=binom" >> $script
-  echo './gph $x $y 100000 $dist 0.01 0.0 0.01 0.02 0.03 0.05 0.07 0.08 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9' >> $script
+  echo './gph $x $y 150000 $dist 0.020 0.0 0.316 0.447 0.548 0.632 0.707 0.775 0.837 0.894 0.949'  >> $script
   chmod +x $script
 
   # write the submit file
